f7691fff01cc2977cb98ecdc74e5221c76ef826d
jennifer <cxxyao2@gmail.com>
2023-07-31 18:17:42 -0400

    Add DbContext

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@ global using dotnet7_sqlserver.Models;
 global using dotnet7_sqlserver.Services.CharacterService;
 global using dotnet7_sqlserver.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+using dotnet7_sqlserver.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using dotnet7_sqlserver.Data;
 
 namespace dotnet7_sqlserver.Services.CharacterService
 {
@@ -10,40 +10,39 @@ namespace dotnet7_sqlserver.Services.CharacterService
   public class CharacterService : ICharacterService
   {
     private readonly IMapper _mapper;
-    public CharacterService(IMapper mapper)
+    private readonly DataContext _context;
+
+    public CharacterService(IMapper mapper, DataContext context)
     {
       _mapper = mapper;
+      _context = context;
     }
-    private static List<Character> characters = new List<Character>
-    {
-      new Character(),
-      new Character { Id = 1, Name = "Sam" }
-    };
 
 
     public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
       var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
       var character = _mapper.Map<Character>(newCharacter);
-      character.Id = characters.Max(c => c.Id) + 1;
-      characters.Add(character);
+      _context.Characters.Add(character);
+      await _context.SaveChangesAsync();
 
-      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+      serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
       return serviceResponse;
     }
 
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
       var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+      var dbCharacters = await _context.Characters.ToListAsync();
+      serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
       return serviceResponse;
     }
 
     public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
     {
       var serviceResponse = new ServiceResponse<GetCharacterDto>();
-      var character = characters.FirstOrDefault(c => c.Id == id);
-      serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+      var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+      serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
 
       return serviceResponse;
     }
@@ -53,7 +52,7 @@ namespace dotnet7_sqlserver.Services.CharacterService
       var serviceResponse = new ServiceResponse<GetCharacterDto>();
       try
       {
-        var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+        var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
         if (character == null)
           throw new Exception($"Character {updatedCharacter.Id} not found");
         character.Name = updatedCharacter.Name;
@@ -63,6 +62,7 @@ namespace dotnet7_sqlserver.Services.CharacterService
         character.Intelligence = updatedCharacter.Intelligence;
         character.Strength = updatedCharacter.Strength;
 
+        await _context.SaveChangesAsync();
         serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
 
       }
@@ -83,12 +83,14 @@ namespace dotnet7_sqlserver.Services.CharacterService
 
       try
       {
-        var character = characters.FirstOrDefault(c => c.Id == id);
-        if(character is null) throw new Exception($"Character with Id {id} not found");
-        characters.Remove(character);
+        var character =await  _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+        if (character is null) throw new Exception($"Character with Id {id} not found");
+        _context.Characters.Remove(character);
+        await _context.SaveChangesAsync();
 
-        serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
-      }catch(Exception ex)
+        serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
+      }
+      catch (Exception ex)
       {
         serviceResponse.Success = false;
         serviceResponse.Message = ex.Message;
